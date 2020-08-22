89bfdbae4e6f1aaed93cc9a6cb3c7d52ab7f09a5
Colin Nolan <cn580@alumni.york.ac.uk>
2020-08-22 02:48:26 +0100

    Provides better error if monitors input is incorrect.

--- filter_plugins/helpers.py
+++ filter_plugins/helpers.py
@@ -11,6 +11,8 @@ class FilterModule(object):
         user_password_map = {user["email"]: user["password"] for user in users}
         processed = []
         for monitor in monitors:
+            if "users" not in monitors:
+                raise ValueError(f"Monitor does not have 'users' list: {monitor}")
             for user_email in monitor["users"]:
                 entry = dict(email=user_email, password=user_password_map[user_email],
                              configuration=monitor["configuration"], id=monitor["id"])
