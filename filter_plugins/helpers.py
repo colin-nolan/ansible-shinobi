975d37c0052cfc42e1b8b8cfdf11122c3cdd45fe
Colin Nolan <cn580@alumni.york.ac.uk>
2020-08-29 23:17:39 +0100

    Corrects bug in users in monitor config check.

--- filter_plugins/helpers.py
+++ filter_plugins/helpers.py
@@ -11,8 +11,8 @@ class FilterModule(object):
         user_password_map = {user["email"]: user["password"] for user in users}
         processed = []
         for monitor in monitors:
-            if "users" not in monitors:
-                raise ValueError(f"Monitor (camera setup) does not have 'users' list: {monitor}")
+            if "users" not in monitor:
+                raise ValueError(f"Monitor (camera setup) does not have a 'users' list: {monitor}")
             for user_email in monitor["users"]:
                 entry = dict(email=user_email, password=user_password_map[user_email],
                              configuration=monitor["configuration"], id=monitor["id"])
